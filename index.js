eb430695a989d15bb2cce39dee4de196902cc2b7
afnannhmg <afnannhmg@outlook.com>
2024-10-07 12:04:27 +0300

    js part 1

---
+++ index.js
@@ -0,0 +1,101 @@
+/**
+ * Task 1:
+ * Create a function named `printName`
+ * - that just prints your name on the screen
+ */
+
+function printName() {
+console.log("Afnan");
+}
+
+/**
+ * Task 2:
+ * Create a function named `printAge`
+ * - that takes a birth year as a parameter,
+ * - and prints the age on the screen.
+ * - Age = current year - birth
+ */
+
+ 
+function printAge() {
+byear = 1998;
+age = 2024 - byear;
+console.log(age);
+}
+
+
+
+/**
+ * Task 3:
+ * Create a function named `printAgeAndName`
+ * - that takes a birth year and your name as parameters,
+ * - and prints 'Hello NAME you are AGE years old' on the screen.
+ * - Age = current year - birth
+ */
+
+function printAgeAndName() {
+    x = "Afnan";
+    byear = 1998;
+    age = 2024 - byear;
+    console.log("Hello" + x + " you are " + age + "years old");
+
+}
+
+ /**
+ * Task 3:
+ * Create a function named `printAgeAndName`
+ * - that takes a birth year and your name as parameters,
+ * - and prints 'Hello NAME you are AGE years old' on the screen.
+ * - Age = current year - birth
+ */
+
+ function printAgeAndName2() {
+    x = "Afnan";
+    byear = 1998;
+ }
+
+ /**
+ * Task 4:
+ * Create a function named `printHello`
+ * - that takes 2 parameters, name, and language
+ * - language can be passed in different values, here are the accepted values:-
+ * -- en: it should print `Hello NAME`
+ * -- es: it should print `Hola NAME`
+ * -- fr: it should print `Bonjour NAME`
+ * -- tr: it should print `Merhaba NAME`
+ */
+
+
+ function printHello() {
+    x = "Afnan";
+    if (language = "en")
+        {console.log("Hello Afnan");
+    }
+
+    else if (language = "es")
+    {console.log("Hola Afnan"); }
+
+    else if (language = "fr")
+    {console.log("Bonjour Afnan"); }
+
+    else (language = "tr")
+        console.log("Merhaba Afnan");
+ }
+ 
+/**
+ * Task 5:
+ * Create a function named `printMax`
+ * - that takes 2 parameters as numbers
+ * - should print out the bigger number
+ */
+
+function printMax() {
+a = 10;
+b = 20;
+if (a>b)
+{console.log(a);}
+else console.log(b);
+}
+
+
+
